c31ad566fbfc15d3d3a9737232f1c5f85a928a80
Srimugdha04 <168437843+Srimugdha04@users.noreply.github.com>
2025-04-14 14:44:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
 # Replace with your own MongoDB connection string if needed
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://srimugdha:<db_password>@cluster0.p71ahgg.mongodb.net/")
 
 # Database and collections
 db = client['todo_app']
